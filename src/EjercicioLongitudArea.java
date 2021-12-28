33dd7baf7fe67532077a89ac3fea09907294b937
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2021-12-28 18:39:22 +0100

    Ejercicio de longitud y area de radio

---
+++ src/EjercicioLongitudArea.java
@@ -0,0 +1,49 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EjercicioLongitudArea {
+	/**
+	 * Diseñar una aplicación que calcule la longitud y el área
+	 * de una circunferencia. Para ello, el usuario debe introducir 
+	 * el radio (que puede contener decimales).
+	 * 
+	 * 						Fórmula: 
+	 * Longitud = 2* PI (3,14) * RADIO 
+	 *			  ÁREA= PI (3,14) * RADIO ^2
+	 *
+	 * 						Programa
+	 * Crear tres variables double llamadas Rad, Log, Area
+	 * Invocar el Scanner y asignarle un nombre 
+	 * Mostrar un syso que pida el radio 
+	 * Guardar el valor en la variable Rad
+	 * 
+	 * Usar la variable Log para realizar el cálculo de la longitud 
+	 * Llamar a la clase Math para calcular PI por el Rad (valor usuario)
+	 * 
+	 * Usar la variable Area
+	 * Llamar a la clase Math para calcular PI, por RADIO, Math POW elevado a 2
+	 * 
+	 * Mostrar el pantalla la longitud y el area
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+	double Rad, Log, Area;
+	
+	Scanner sc=new Scanner(System.in);
+	sc.useLocale(Locale.US);
+	
+	System.out.println("Digite el radio a calcular: ");
+	Rad=sc.nextDouble();
+	
+	Log= 2*Math.PI*Rad;
+	Area= Math.PI *Math.pow(Rad,2);
+	
+	System.out.println("La longitud es: " +Log);
+	
+	System.out.println("El área es: "+ Area);
+	
+
+	}
+
+}
